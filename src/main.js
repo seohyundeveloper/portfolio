7065815e70cea392bb63d6933836188307e0ff3f
seohyundeveloper <seoandjisister77@gmail.com>
2023-08-08 00:11:26 +0900

    JS-home

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
-// Header에 페이지 아래로 스크롤시 다크 스타일링 적용
+// Header에 페이지 아래로 스크롤시 다크 스타일링 적용.
 const header = document.querySelector('.header');  // Header's Height
-const headerHeight = header.getBoundingClientRect().height 
+//const headerHeight = header.getBoundingClientRect().height 
+const headerHeight = header.offsetHeight;
 document.addEventListener( 'scroll', () => {
-  if(window.scrollY > headerHeight) {
+  if ( window.scrollY > headerHeight ) {
     header.classList.add('header--dark');
   } else {
     header.classList.remove('header--dark');
   }
 });
+
+//Home 섹션을 아래로 스크롤시 투명하게 적용.
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1-window.scrollY / homeHeight;
+})
