0238a383a8f1fc3b6d545fee4d08256580ee772b
seohyundeveloper <seoandjisister77@gmail.com>
2023-08-12 01:47:09 +0900

    style-change3

--- src/type.js
+++ src/type.js
@@ -2,14 +2,14 @@
 
 new TypeIt('.home__title__strong', {
   loop: true,
-  speed: 100
+  speed: 110
 }) // Dream Developer
-.move(-15)
-.type('Amazing ') // Amazing |Front-End Developer
-.pause(1000)
-.move(null, {to: 'END'})
-.delete() 
-.type('Front-End Developer ') // Front-End Developer|
-.pause(1000)
-.delete()
+    .move(-15)
+    .type('Amazing ') // Amazing |Front-End Developer
+    .pause(1000)
+    .move(null, {to: 'END'}) // Amazing Front-End Developer|
+    .delete() 
+    .type('Front-End Developer ') // Front-End Developer|
+    .pause(1500)
+    .delete()
     .go();
